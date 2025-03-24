9a5896833b289da9d0cf89904f8aaea8f21fe205
BoyWhy Chen <flowercatswets@yahoo.com.tw>
2025-03-24 17:25:51 +0800

    Gray Filter via css

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var express=require('express');
 var app=express();
 console.log('server started');
-
-
+//Load css file in /public folder
+app.use(express.static(__dirname+'/public'));
 
 app.get('/',function(req,res){
 	res.render('index.ejs');
